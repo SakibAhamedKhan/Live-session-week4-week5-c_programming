26d6c0540693dc64e7422080a6c2e6c18f89536d
SakibAhamedKhan <Sakibahamedkhan01@gmail.com>
2023-05-22 21:50:05 +0600

    first commit

---
+++ 1. Nested Loop/problem1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+
+    int space=n-1;
+    int star=1;
+    for(int i=1; i<=n; i++){
+        // space print
+        for(int j=1; j<=space; j++){
+            printf(" ");
+        }
+
+        // star print
+        for(int j=1; j<=star; j++){
+            printf("*");
+        }
+        printf("\n");
+
+        space = space -1;
+        star = star + 2;
+
+    }
+
+    return 0;
+}
---
+++ 2. Function/functionTypes.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int a=10,b=20;
+
+    int c = a+b;
+
+    printf("The sum is: %d\n",c);
+
+    return 0;
+}
